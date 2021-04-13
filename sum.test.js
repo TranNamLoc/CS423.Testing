931b00668c865d1db37a1e645ae079b3cf819248
Trần Bảo Phúc <tranbphuc06041999@gmail.com>
2021-04-13 22:02:33 +0700

    test

--- sum.test.js
+++ sum.test.js
@@ -4,7 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-
-test('adds 2*2 to equal 4', () => {
+test('adds 1 + 7 to equal 8', () => {
     expect(sum(1, 7)).toBe(8);
 });
